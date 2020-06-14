32925028aec39155944727b8338234e719fc526a
jh <lyn00227@naver.com>
2020-06-14 12:51:06 +0900

    200612

--- server/routes/board.js
+++ server/routes/board.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { getBoardList, getBoardContent, boardPostRegister, getPostNum, insertContent } = require('../models/Board');
-
+const pool = require('../config/pool')
 
 router.get("/openpage", (req, res) => {
 
@@ -24,11 +24,11 @@ router.post("/postnum", (req, res) => {
                 
                 return res.status(200).json( {success: true, content, postnum:postNum} )
             })
-    
+
 });
 
 //return res.status(200).json( {success: true} )
-router.post("/createPost", (req, res) => {
+router.post("/createPost2", (req, res) => {
 
     const post = req.body
 
@@ -48,6 +48,40 @@ router.post("/createPost", (req, res) => {
             })
         })       
     })
+});
+
+//select문 두 개 쓰는 라우터
+router.post("/createPost", async (req, res) => {
+
+    const post = req.body;
+    const conn = await pool.getConnection();
+    var e = false;
+
+    try {
+        await conn.beginTransaction();
+
+        const ins_1 = await conn.query('INSERT INTO `BulletinBoard`.`PostInfo` (`title`, `writer`, `date`, `views`, `favorite`) VALUES (?, ?, ?, ?, ?)', [post.title, post.writer, post.date, 0, 0]);
+        //console.log("ins_1 출력:     ", ins_1);
+
+        const [postNum] = await conn.query('SELECT postnum FROM BulletinBoard.PostInfo WHERE writer = ? and date = ?', [post.writer, post.date]);
+        //console.log("postNum 출력:     ", postNum[0].postnum);
+        
+        const ins_2 = await conn.query('INSERT INTO `BulletinBoard`.`PostContents` (`pNum`, `pContent`) VALUES (?, ?)', [postNum[0].postnum, post.pContent]);
+        //console.log("ins_2 출력:     ", ins_2);
+
+        await conn.commit();
+
+    } catch (err) {
+        //console.log("에러가 발생했어요~~!!", err);
+        e = true;
+        conn.rollback();
+    } finally {
+        conn.release();
+
+        if (e) return res.status(400).json( { success: false, err } );
+
+        return res.status(200).json( {success: true} );
+    }
 });
 
 module.exports = router;
